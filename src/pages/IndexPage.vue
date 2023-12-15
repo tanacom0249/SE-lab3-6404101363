3166816c2632bb426470f38fe19871dd90635448
tanacom0249 <119926673+tanacom0249@users.noreply.github.com>
2023-12-15 11:16:16 +0700

    Thanakom Quangean 6404101363  15/12/2566

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -14,6 +14,14 @@
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'กรุณาใส่ชื่อและนามสกุล']"
       />
+      <q-input
+        filled
+        v-model="name"
+        label="yourname *"
+        hint="Name and lastname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please Enter your name and lastname ']"
+      />
 
       <q-input
         filled
